09aea95c44491978b322c3a5443d6dccf692bf59
Sathish2909 <sathishshanmugam2904@gmail.com>
2024-12-06 12:29:22 +0530

    change

--- 11millioncrafts/frontend/src/components/About.jsx
+++ 11millioncrafts/frontend/src/components/About.jsx
@@ -3,7 +3,7 @@ function About() {
     return (
       <div className="page-content">
         <h2>About Us</h2>
-        <p> Welcome to a world where gifting isn’t just an exchange; it’s an art form, a language of appreciation, and a bridge that connects hearts across the corporate landscape. At 11Millioncraft, we specialize in curating exceptional gifting solutions that transcend the ordinary, leaving a trail of smiles and lasting impressions. Every gift tells a story, and we’re here to help you craft unforgettable moments for the people you cherish the most.</p>
+        <p> Welcome to a world where gifting isn’t just an exchange; it’s an art form, a language of appreciation, and a bridge that connects hearts across the corporate landscape. At 11Millioncraft, we specialize in curating exceptional gifting solutions that transcend the ordinary, leaving a trail of smiles and lasting impressions. Every gift tells a story, and we’re here to help you craft unforgettable moments for the people you cherish the most.sundar lk</p>
       </div>
     );
   }
